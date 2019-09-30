d42070797e3d9a7fa48c1c31b54b03bd93cfcc6a
Juanxhocm <juanmcm2oo1@gmail.com>
2019-09-30 08:44:16 -0500

    mi aporte es un trabajo y aumente usuario

---
+++ src/principal/usuario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author ALUMNO - FIIS
+ */
+public class usuario {
+    
+    private String Nombre;
+    private String apellidos;
+    private int edad;
+
+    public usuario(String Nombre, String apellidos, int edad) {
+        this.Nombre = Nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+    
+}
